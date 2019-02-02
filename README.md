3741c1af959366b6f3ad1c08a3f0fec95a49ecb2
winescout <matt@notch8.com>
2019-02-01 17:59:32 -0800

    initial commit of front and backend

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # intro-to-authentication-challenge
+
+## Getting Started
+
+### Clone the Rails API
+
+We have created a rails backend app with Devise authentication already hooked up. We will go though all the steps of setting up a rails app with Devise later.
+
+For now, go to this repo:
+
+<a href="https://github.com/2018-Delta/example-auth-backend" target="_blank">example-auth-backend</a>
+
+Clone the repo and start the app. Remember all the steps for setting up a rails app!
+
+### Clone the React App
+
+We have also created the beginnings of a React front end with a registration page. It is already set to work with the rails api.
+
+Go to the frontend repo <a href="https://github.com/2018-Delta/example-auth-frontend" target="_blank">example-auth-frontend</a>
+
+Clone the repo and start the app. Remember all the steps for setting up a React app!
+
+### Get Aquainted
+
+Take a few moments to look through the React app and the register page. Check to see if you can follow the path of authentication
+
+
+### Challenges
+
+- create a public page with content
+- create a protected page with content
+- add a Login page with Login functionality
+- add a header with a logout button
+
+### Stretch Challenges
+- edit the header to show a logout button when user is logged in and a log in button when user is guest
+- create a dashboard that shows all information for the logged in user
---
+++ example-auth-frontend/src/services/index.js
@@ -0,0 +1,99 @@
+import decode from 'jwt-decode'
+
+export default class AuthService {
+	constructor(domain) {
+		this.domain = domain || 'http://localhost:3001'
+	}
+
+	login = (email, password) => {
+		return this.authFetch(`${this.domain}/users`, {
+			method: "POST",
+			body: JSON.stringify(email, password),
+		})
+		.then(res => {
+			this.setToken(res.jti)
+			return res
+		})
+	}
+
+	register = (user) => {
+		return this.fetch(`${this.domain}/users`, {
+			method: "POST",
+			body: JSON.stringify(user),
+		})
+		.then(res => {
+			this.setToken(res.jti)
+			return res
+		})
+	}
+
+	loggedIn() {
+		const token = this.getToken()
+		return !!token && !this.isTokenExpired(token)
+	}
+
+	isTokenExpired(token) {
+		try {
+			const decoded = decode(token)
+			if (decoded.exp < Date.now() / 1000) {
+				return true
+			} else {
+				return false
+			}
+		}
+		catch (err) {
+			return false;
+		}
+	}
+
+	// The token is stored in the browser
+	setToken(idToken) {
+		localStorage.setItem('id_token', idToken)
+	}
+
+	// Fetch the token from local storage
+	getToken() {
+		return localStorage.getItem('id_token')
+	}
+
+	// Removes the token
+	logout() {
+		localStorage.removeItem('id_token');
+	}
+
+	getUserId = () => {
+		const token = decode(this.getToken());
+		return token.sub
+	}
+
+	authFetch = (url, options) => {
+		const headers = {
+			'Accept': 'application/json',
+			'Content-Type': 'application/json'
+		}
+
+		if (this.loggedIn()) {
+			headers['Authorization'] = 'Bearer ' + this.getToken()
+		}
+
+		return fetch(url, {
+			headers,
+			...options
+		})
+		.then(this._checkStatus)
+		.then(response => response.json())
+		.catch(err => {
+			console.log("::: FETCH ERROR CAUGHT:::", err)
+			return err
+		})
+	}
+
+	_checkStatus(response) {
+		if(response.status >= 200 && response.status < 300) {
+			console.log(":::SUCCESS:::");
+		} else {
+			console.log(":::ERROR:::", response)
+		}
+		return response
+	}
+}
